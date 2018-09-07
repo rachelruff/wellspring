499ed3cdc309dbedf968c80a4b197126a7d06145
rachelruff <rachelruff@gmail.com>
2018-09-07 15:26:10 -0600

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,22 @@
-import React from 'react';
-import logo from '../assets/logo.png';
-import './Header.css'
+import React from "react";
+import logo from "../assets/logo.png";
+import "./Header.css";
 
 const Header = props => {
-    return (
-        <div className='header-container'>
-        <img className='header-container--logo' src={logo} alt='wellspring realty'/>
-            <ul>
-                <li>
-                    Home
-            </li>
-                <li>
-                    Featured Properties
-            </li>
-                <li>
-                    About us
-            </li>
-                <li>
-                    Contact
-            </li>
-                <li>
-                    Charities
-            </li> 
-            </ul>
-
-        </div>
-    )
-}
+  return (
+    <div class="header">
+      <div class="header__logo-box">
+        <img src={logo} alt="Wellspring Realty" class="header__logo" />
+      </div>
+      <ul className="header--nav">
+        <li>Home</li>
+        <li>Featured Properties</li>
+        <li>About us</li>
+        <li>Contact</li>
+        <li>Charities</li>
+      </ul>
+    </div>
+  );
+};
 
 export default Header;
